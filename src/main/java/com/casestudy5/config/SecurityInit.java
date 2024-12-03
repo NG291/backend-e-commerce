bd07882b9ac8dc6caaa605409fa5a141d2401d07
NG291 <you@example.com>
2024-12-03 09:53:39 +0700

    cap nhat

--- src/main/java/com/casestudy5/config/SecurityInit.java
+++
@@ -1,7 +0,0 @@
-package com.casestudy5.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
