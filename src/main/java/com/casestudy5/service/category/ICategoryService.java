0cd6df2bd2632c3cf354f0a13d6922ea551cedda
NG291 <you@example.com>
2024-12-23 10:51:18 +0700

    update readme

--- src/main/java/com/casestudy5/service/category/ICategoryService.java
+++ src/main/java/com/casestudy5/service/category/ICategoryService.java
@@ -1,9 +1,1 @@
-package com.casestudy5.service.category;
 
-import com.casestudy5.model.entity.category.Category;
-import com.casestudy5.service.IGenerateService;
-
-public interface ICategoryService extends IGenerateService<Category> {
- Category findByName(String name);
- 
-}
